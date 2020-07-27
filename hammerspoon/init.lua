89989dbf438698bc305902a23de00b432494ad5c
Andrew Winter <awinter@camsbycbs.com>
2020-07-27 07:55:59 -0400

    hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,38 +1,26 @@
-hs.urlevent.bind(
-    "someAlert",
-    function(eventName, params)
-        hs.alert.show("Received someAlert")
-    end
-)
 
-hs.hotkey.bind(
-    {"cmd", "alt", "ctrl"},
-    "W",
-    function()
-        hs.notify.new({title = "Hammerspoon", informativeText = "Hello World"}):send()
-    end
-)
 
 function devLayout(eventName, params)
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "vpn: Start CBS"')
     hs.execute('/usr/local/bin/code -n "/Users/baconcheesefry/code/vscode/ib-ng.code-workspace"')
     hs.execute('/usr/local/bin/code -n "/Users/baconcheesefry/code/vscode/ib-laravel.code-workspace"')
     hs.application.launchOrFocus("Safari")
     hs.application.launchOrFocus("Slack")
-    hs.application.launchOrFocus("iTerm2")
+    hs.application.launchOrFocus("iTerm")
     hs.application.launchOrFocus("Trello")
     hs.application.launchOrFocus("Microsoft Teams")
     hs.application.launchOrFocus("Sococo")
     hs.application.launchOrFocus("Airmail")
     hs.application.launchOrFocus("Sonos")
 
-    local laravelName = hs.window.find("ib%-laravel"):title()
-    local ngName = hs.window.find("ib%-ng"):title()
+    -- local laravelName = hs.window.find("ib%-laravel"):title()
+    -- local ngName = hs.window.find("ib%-ng"):title()
     local laptopScreen = "Color LCD"
     local bigScreen = "U4008B"
     local windowLayout = {
         {"Safari", nil, bigScreen, {x = 0.37, y = 0, w = 0.37, h = 0.5}, nil, nil},
-        {nil, laravelName, bigScreen, {x = 0, y = 0.5, w = 0.37, h = 0.5}, nil, nil},
-        {nil, ngName, bigScreen, {x = 0, y = 0, w = 0.37, h = 0.5}, nil, nil},
+        -- {nil, laravelName, bigScreen, {x = 0, y = 0.5, w = 0.37, h = 0.5}, nil, nil},
+        -- {nil, ngName, bigScreen, {x = 0, y = 0, w = 0.37, h = 0.5}, nil, nil},
         {"Slack", nil, bigScreen, {x = 0.74, y = 0, w = 0.26, h = 0.25}, nil, nil},
         {"iTerm2", nil, bigScreen, {x = 0.74, y = 0.25, w = 0.26, h = 0.25}, nil, nil},
         {"Trello", nil, bigScreen, {x = 0.74, y = 0.5, w = 0.26, h = 0.25}, nil, nil},
@@ -41,12 +29,98 @@ function devLayout(eventName, params)
         {"Airmail", nil, laptopScreen, {x = 0.12, y = 0.12, w = 0.8, h = 0.5}, nil, nil},
         {"Sonos", nil, laptopScreen, {x = 0.2, y = 0.15, w = 0.6, h = 0.7}, nil, nil}
     }
-    hs.layout.apply(windowLayout)
+    if(hs.screen.find(bigScreen)) then
+        hs.layout.apply(windowLayout)
+    end
+
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Angular"')
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Angular"')
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Angular"')
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel"')
+    -- hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel Queue default"')
+    -- hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel Queue nonpbf"')
 end
-
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", devLayout)
-
 hs.urlevent.bind("dev-layout", devLayout)
+
+
+function launchLaravelQueueDefault(eventName, params)
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel Queue default"')
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel Queue nonpbf"')
+end
+hs.urlevent.bind("laravel-queue-default", launchLaravelQueueDefault)
+
+-- function launchLaravelQueueNonPbf(eventName, params)
+--     hs.applescript('tell application "Keyboard Maestro Engine" to do script "iTerm: profile: IB Laravel Queue nonpbf"')
+-- end
+-- hs.urlevent.bind("laravel-queue-nonpbf", launchLaravelQueueNonPbf)
+
+
+function doneWorking(eventName, params)
+    hs.applescript('tell application "Keyboard Maestro Engine" to do script "vpn: Stop CBS"')
+    local closeable = hs.application.get('iTerm2')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('Trello')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('Sococo')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('Microsoft Teams')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('Code')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('Sourcetree')
+    if (closeable ) then
+        closeable:kill()
+    end
+    closeable = hs.application.get('TablePlus')
+    if (closeable ) then
+        closeable:kill()
+    end
+end
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", doneWorking)
+hs.urlevent.bind("dev-un-layout", doneWorking)
+
+
+function usbDeviceCallback(data)
+    if (data["productName"] == "iPhone") then
+        if (data["eventType"] == "added") then
+            devLayout()
+        end
+    end
+end
+usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
+
+
+function moveCodeWindow(codeWindow)
+    hs.alert.show("code window created")
+    local laptopScreen = "Color LCD"
+    local bigScreen = "U4008B"
+    local position = {x = 0, y = 0.5, w = 0.37, h = 0.5}
+    if(codeWindow:title():find("ib%-ng")) then
+        position = {x = 0, y = 0, w = 0.37, h = 0.5}
+    end
+
+    local windowLayout = {
+        {nil, codeWindow, bigScreen, position, nil, nil}
+    }
+    if(hs.screen.find(bigScreen)) then
+        hs.layout.apply(windowLayout)
+    end
+end
+local wf=hs.window.filter
+wf_code = wf.new(false):setAppFilter('Code',{allowTitles={'ib%-laravel %(Workspace%)','ib%-ng %(Workspace%)'}})
+wf_code:subscribe(wf.windowCreated,moveCodeWindow, true)
 
 function reloadConfig(files)
     doReload = false
@@ -61,3 +135,6 @@ function reloadConfig(files)
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded again again")
+
+
+
